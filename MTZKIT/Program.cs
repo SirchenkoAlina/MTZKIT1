089c7f3de1e56c2054611f138dd8cb58ff8fa292
Alina Sirchenko <sirchenko.alina@vu.cdu.edu.ua>
2020-12-20 21:01:04 +0200

    Створено найпростіший консольний додаток

---
+++ MTZKIT/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MTZKIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
