14d98bbaf60c7d72e696ffdaeae9ea2791e29a29
Alina Sirchenko <sirchenko.alina@vu.cdu.edu.ua>
2020-12-20 21:17:09 +0200

    Додано підрахунок суми чисел і виведення результату

--- MTZKIT/Program.cs
+++ MTZKIT/Program.cs
@@ -6,7 +6,10 @@ namespace MTZKIT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 10;
+            int y = 10;
+
+            Console.WriteLine("10 + 10 ={0}", x + y);
             Console.ReadKey();
         }
     }
